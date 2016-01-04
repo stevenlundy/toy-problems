ad72df1084874ca62eb1829acebc1e1fe872486e
Steven Lundy <steven.lundy@gmail.com>
2016-01-04 13:07:22 -0500

    Write iterative solution to nthFibonacci in Java

--- nthFibonacci/nthFibonacci.java
+++ nthFibonacci/nthFibonacci.java
@@ -1,16 +1,38 @@
 public class nthFibonacci {
+
   public static void main(String[] args) {
     int n = Integer.parseInt(args[0]);
-    System.out.println(getNthFibonacci(n));
+    System.out.println(getNthFibonacciIterative(n));
   }
 
-  public static int getNthFibonacci(int n) {
+  public static int getNthFibonacciRecursive(int n) {
     if (n == 0) {
       return 0;
     } else if (n == 1) {
       return 1;
     } else {
-      return getNthFibonacci(n - 1) + getNthFibonacci(n - 2);
+      return getNthFibonacciRecursive(n - 1) + getNthFibonacciRecursive(n - 2);
     }
   }
+
+  public static int getNthFibonacciIterative(int n) {
+    if (n == 0) {
+      return 0;
+    } else if (n == 1) {
+      return 1;
+    }
+
+    int twoBack = 0;
+    int oneBack = 1;
+    int temp;
+
+    for (int i = 2; i < n; i++) {
+      temp = oneBack;
+      oneBack = oneBack + twoBack;
+      twoBack = temp;
+    }
+
+    return oneBack + twoBack;
+  }
+
 }
